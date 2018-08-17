47939f21ff52f2a67381c80bdd18e9f200dee61e
Webley <webadict@gmail.com>
2018-08-17 09:56:33 -0500

    Added DeckCondition
    - DeckCondition allows for all cards to be tested with a set of conditions.
    - Re-ordered ConditionArg enum.

---
+++ game/src/main/java/net/demilich/metastone/game/spells/desc/condition/DeckCondition.java
@@ -0,0 +1,28 @@
+package net.demilich.metastone.game.spells.desc.condition;
+
+import net.demilich.metastone.game.GameContext;
+import net.demilich.metastone.game.Player;
+import net.demilich.metastone.game.cards.Card;
+import net.demilich.metastone.game.entities.Entity;
+import net.demilich.metastone.game.spells.desc.filter.EntityFilter;
+
+public class DeckCondition extends Condition {
+
+	public DeckCondition(ConditionDesc desc) {
+		super(desc);
+	}
+
+	@Override
+	protected boolean isFulfilled(GameContext context, Player player, ConditionDesc desc, Entity source, Entity target) {
+		Condition[] conditions = (Condition[]) desc.get(ConditionArg.CONDITIONS);
+		for (Card card : player.getDeck()) {
+			for (Condition condition : conditions) {
+				if (!condition.isFulfilled(context, player, source, card)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+}
